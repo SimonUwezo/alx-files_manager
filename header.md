16ac9b02e6a620f9d9d0544547c9dc0488bb4938
SimonUwezo <simonnmwangi@gmail.com>
2024-10-02 12:35:11 -0700

    Filesmanager

--- header.md
+++
@@ -1,2 +0,0 @@
-
-This is a simple RESTful API that allows users to upload files, retrieve information about the files, retrieve the file data and share uploaded files with other users
--- utils/db.js
+++ utils/db.js
@@ -1,58 +1,38 @@
 import { MongoClient } from 'mongodb';
 
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}/`;
+
 class DBClient {
-  /**
-   * Initializes a new instance of DBClient
-   */
   constructor() {
-    const HOST = process.env.DB_HOST || 'localhost';
-    const PORT = process.env.BD_PORT || 27017;
-    const DATABASE = process.env.DB_DATABASE || 'files_manager';
-    const URI = `mongodb://${HOST}:${PORT}`;
-    this.mongoClient = new MongoClient(URI, { useUnifiedTopology: true });
-    this.mongoClient.connect((error) => {
-      if (!error) this.db = this.mongoClient.db(DATABASE);
+    this.db = null;
+    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
+      if (error) console.log(error);
+      this.db = client.db(database);
+      this.db.createCollection('users');
+      this.db.createCollection('files');
     });
   }
 
-  /**
-   * Check mongodb client's connection status
-   * @returns {boolean} mongoClient connection status
-   */
   isAlive() {
-    return this.mongoClient.isConnected();
-  }
-
-  /**
-   * Retrieves specified collection from database
-   * @returns {import("mongodb").Collection} - users collection object
-   */
-  getCollection(collectionName) {
-    const collection = this.db.collection(collectionName);
-    return collection;
+    return !!this.db;
   }
 
   async nbUsers() {
-    const usersCollection = this.getCollection('users');
-    const numberOfUsers = await usersCollection.countDocuments();
-    return numberOfUsers;
+    return this.db.collection('users').countDocuments();
   }
 
-  /**
-   * Queries 'files' collection
-   * @returns {number} - number of documents in files collection
-   */
+  async getUser(query) {
+    console.log('QUERY IN DB.JS', query);
+    const user = await this.db.collection('users').findOne(query);
+    console.log('GET USER IN DB.JS', user);
+    return user;
+  }
+
   async nbFiles() {
-    const filesCollection = this.getCollection('files');
-    const numberOfFiles = filesCollection.countDocuments();
-    return numberOfFiles;
-  }
-
-  /**
-   * Closes connection to mongodb client
-   */
-  async close() {
-    await this.mongoClient.close();
+    return this.db.collection('files').countDocuments();
   }
 }
 
